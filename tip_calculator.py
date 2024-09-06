b9b1042f5c8e0560a75bd342d6b0c2806d77a6d3
atia-habib <156105469+atia-habib@users.noreply.github.com>
2024-09-06 20:58:08 +0530

    Update and rename index.html to tip_calculator.py

---
+++ tip_calculator.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator!")
+totalbill=float(input("What was the total bill? $" ))
+tip=float(input("How much tip would you like to give? 10, 12, or 15?"))
+no_of_people=int(input("How many people to split the bill with? "))
+totalbill=totalbill+((tip/100)*totalbill)
+ind_bill=round(totalbill/no_of_people,2)
+print(f"Each person has to pay {ind_bill}")
