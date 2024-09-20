563f5c6a8231c70cc237a3d91f1f722feb70fd7a
atia-habib <156105469+atia-habib@users.noreply.github.com>
2024-09-20 10:55:58 +0530

    Update ceaser_cipher.py

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -1,5 +1,5 @@
 
-from art import logo
+from ceaser_art import logo
 print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
